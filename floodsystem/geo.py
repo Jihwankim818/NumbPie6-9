4a919307b3048003bf7cd83ef52cfa44573b476f
Jihwan Kim <jk818@cam.ac.uk>
2022-02-02 12:43:34 +0000

    1234

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -5,6 +5,7 @@
 geographical data.
 
 """
+
 from pickle import TUPLE1
 from floodsystem.station import MonitoringStation
 from haversine import haversine
@@ -47,11 +48,27 @@ def stations_by_river(stations):
             RiverStat['River Cam'] = RiverStat['River Cam'] + ' ' + '{},'.format(stations[n].name)
     x = RiverStat['River Cam']
     return x
+
+def rivers_by_station_number(stations, N):
+    river2 ={}
+    for n in range(len(stations)):
+        if stations[n].river not in river2:
+             river2.update({stations[n].river: 1})
+        elif stations[n].river in river2:
+             river2[stations[n].river] = river2[stations[n].river] + 1
+
     
+    rivernumpy = river2.items()
+    rivernumppy = list(rivernumpy)
+    rivernumppy.sort(key = lambda x:x[1], reverse = True)
+    return rivernumppy[:N+1]
+
+   
 
 
+    
+        
 
-
-
-
+    
+    
 
