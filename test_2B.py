06f34c5462cc0a765b35ad955a02591150b397d9
SBamRittan <samjbrittan@gmail.com>
2022-02-07 09:59:41 +0000

    Haversine in yml

--- test_2B.py
+++ test_2B.py
@@ -1,6 +1,7 @@
 from floodsystem.flood import stations_level_over_threshold
 from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.station import MonitoringStation
+from haversine import haversine
 stations = build_station_list()
 update_water_levels(stations)
 
